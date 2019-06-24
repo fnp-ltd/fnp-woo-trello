99f504aa5431cf65ea5addf925decb01fd66b69b
Clive <52170133+fnp-ltd@users.noreply.github.com>
2019-06-24 16:56:06 +0100

    Update fnp-woo-trello.php

--- fnp-woo-trello/fnp-woo-trello.php
+++ fnp-woo-trello/fnp-woo-trello.php
@@ -1,1 +1,13 @@
 <?php
+/**
+ * Plugin Name: fnp-woo-trello
+ * Plugin URI:  https://github.com/fnp-ltd/fnp-woo-trello
+ * Description: WooCommerce to Trello
+ * Version:     0.0.1
+ * Author:      Functional Paradigms Ltd
+ * Author URI:  http://fnp.ltd
+ * Text Domain: fnpwootre
+ * Domain Path: /languages
+ * License:     GPL3
+ */
+
